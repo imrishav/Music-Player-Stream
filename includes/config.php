44062ae2b84762c743be8b858fe4da85111951b7
Rishav Sinha <Rishav Sinha>
2018-03-27 01:41:06 +0530

    vsd

--- includes/config.php
+++ includes/config.php
@@ -5,11 +5,22 @@
 
 	$timezone = date_default_timezone_set("Asia/Kolkata");
 
-	$con = mysqli_connect("localhost:3310","root","","spotify");
 
-	if(mysqli_connect_errno()){
-		echo "Failed to Connect " . mysqli_connect_errno();
-	}
+		$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+		 
+		$server = $url["us-cdbr-iron-east-05.cleardb.net"];
+		$username = $url["ba13ac2ea1fc64"];
+		$password = $url["0090cde6"];
+		$db = substr($url["heroku_fb7c8e9620d0bf1"], 1);
+		 
+		$con = new mysqli($server, $username, $password, $db);
 
 
+	// $con = mysqli_connect("localhost:3310","root","","spotify");
+
+	// if(mysqli_connect_errno()){
+	// 	echo "Failed to Connect " . mysqli_connect_errno();
+	// }
+
+	// :\xampp\mysql --host=us-cdbr-iron-east-05.cleardb.net --user=ba13ac2ea1fc64 --password=0090cde6 --reconnect heroku_fb7c8e9620d0bf1< spotify.sql	
 ?>
